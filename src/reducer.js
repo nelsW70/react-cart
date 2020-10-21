9f758e9047b2def93ef1c34c9123b35a996bb3ad
Nelson Foltz <nelsfoltz@gmail.com>
2020-10-21 10:00:57 -0400

    add: remove single item from cart

--- src/reducer.js
+++ src/reducer.js
@@ -2,6 +2,11 @@ const reducer = (state, action) => {
   switch (action.type) {
     case 'CLEAR_CART':
       return { ...state, cart: [] }
+    case 'REMOVE':
+      return {
+        ...state,
+        cart: state.cart.filter(cartItem => cartItem.id !== action.payload)
+      }
     default:
       return state
   }
